d7c164056dd0f1009f8ce90d981705d21d718f8b
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-11-20 15:37:46 +0200

    100 / 100

---
+++ 02_LinearDataStructures/Lab/Problem02.Stack/Stack.cs
@@ -0,0 +1,112 @@
+﻿namespace Problem02.Stack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Stack<T> : IAbstractStack<T>
+    {
+        private class Node
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+
+            public T Value { get; set; }
+
+            public Node Next { get; set; }
+        }
+
+        private Node top;
+
+        public int Count { get; set; }
+
+        public void Push(T item)
+        {
+            Node node = new Node(item);
+
+            if (this.top == null)
+            {
+                this.top = node;
+            }
+            else
+            {
+                node.Next = this.top;
+                this.top = node;
+            }
+
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            this.CheckForEmptyStack();
+
+            T value = this.top.Value;
+
+            if (this.top.Next == null)
+            {
+                this.top = null;
+            }
+            else 
+            { 
+                Node newTop = this.top.Next;
+                this.top = newTop;
+            }
+
+            this.Count--;
+
+            return value;
+        }
+
+        public T Peek()
+        {
+            this.CheckForEmptyStack();
+
+            return this.top.Value;
+        }
+
+        public bool Contains(T item)
+        {
+            Node currentItem = this.top;
+
+            while (currentItem != null)
+            {
+                if (currentItem.Value.Equals(item))
+                {
+                    return true;
+                }
+
+                currentItem = currentItem.Next;
+            }
+
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node currentItem = this.top;
+
+            while (currentItem != null)
+            {
+                yield return currentItem.Value;
+
+                currentItem = currentItem.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+        
+        private void CheckForEmptyStack()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+        }
+    }
+}
