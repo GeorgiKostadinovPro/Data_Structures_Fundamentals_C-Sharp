592db34a5b69cb5fecb2ae6ae6cb8657f692fe5e
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-12-13 23:05:16 +0200

    tree representation using class Tree<T> 100/100

---
+++ 03_TreesRepresentationAndTraversal_(BFS, DFS)/Lab/Tree/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Tree<int> mainTree = new Tree<int>(34,
+                                     new Tree<int>(36,
+                                          new Tree<int>(42),
+                                          new Tree<int>(3),
+                                               new Tree<int>(78)),
+                                     new Tree<int>(1,
+                                          new Tree<int>(6)),
+                                     new Tree<int>(103)
+                                  );
+
+            Console.WriteLine(string.Join(", ", mainTree.OrderBfs()));
+
+            mainTree.Swap(1, 36);
+
+            Console.WriteLine(string.Join(", ", mainTree.OrderBfs()));
+        }
+    }
+}
