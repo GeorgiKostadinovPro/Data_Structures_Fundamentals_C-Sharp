9cee30a3492408852d34aa1d1f0d6782bf431bec
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-12-16 20:37:59 +0200

    solution file added playground for testing

---
+++ 04_Heaps_BST/Exercises/Playground/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using _03.MinHeap;
+using _04.CookiesProblem;
+
+namespace Playground
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var priorityQueue = new PriorityQueue<int>();
+
+            priorityQueue.Enqueue(4);
+            priorityQueue.Enqueue(7);
+            priorityQueue.Enqueue(2);
+            priorityQueue.Enqueue(9);
+            priorityQueue.Enqueue(1);
+        }
+    }
+}
